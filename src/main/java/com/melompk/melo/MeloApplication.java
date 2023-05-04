7f3ae319155ec6f950ea6f6bbcdf6321230db441
rafal.przypek@student.uj.edu.pl <shadow0bit@DESKTOP-7K0ITBL>
2023-05-04 11:23:46 +0200

    bare-bones implementation of basic GUI (control buttons, volume slider, progress bar)

---
+++ src/main/java/com/melompk/melo/MeloApplication.java
@@ -0,0 +1,31 @@
+package com.melompk.melo;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MeloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MeloApplication.class.getResource("front-page-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 960, 600);
+        stage.setResizable(false);
+        stage.setTitle("MeloZone");
+        stage.setScene(scene);
+        stage.show();
+
+        //On exit
+        stage.setOnCloseRequest(windowEvent -> {
+            Platform.exit();
+            System.exit(0);
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
