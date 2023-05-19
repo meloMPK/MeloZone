e6bf96788277911a907a7184383635fb1b3d4161
akwasniewski <akwasniewski.pl@gmail.com>
2023-05-19 16:11:40 +0200

    added: methods to get all songs from album/artist

--- src/main/java/com/melompk/melo/GetData.java
+++ src/main/java/com/melompk/melo/GetData.java
@@ -15,4 +15,18 @@ public class GetData {//Model
         }
         return songs;
     }
+    public LinkedList<Song> GetAllSongsFromArtist(String artistId) throws ExecutionException, InterruptedException {
+        LinkedList<Song> songs = new LinkedList<>();
+        for(QueryDocumentSnapshot song:FirebaseHandler.db.collection("Songs").whereEqualTo("ArtistId", artistId).get().get()){
+            songs.add(new Song((String) song.get("Name"), (String) song.get("AlbumId"), (String) song.get("ArtistId"), song.getId()));
+        }
+        return songs;
+    }
+    public LinkedList<Song> GetAllSongsFromAlbum(String albumId) throws ExecutionException, InterruptedException {
+        LinkedList<Song> songs = new LinkedList<>();
+        for(QueryDocumentSnapshot song:FirebaseHandler.db.collection("Songs").whereEqualTo("AlbumId", albumId).get().get()){
+            songs.add(new Song((String) song.get("Name"), (String) song.get("AlbumId"), (String) song.get("ArtistId"), song.getId()));
+        }
+        return songs;
+    }
 }
