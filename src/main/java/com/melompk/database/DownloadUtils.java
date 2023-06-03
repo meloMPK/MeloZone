b47a9c2292f80e5e799a13b76ea3765495daea23
AugustynM <augustyn.majtyka@student.uj.edu.pl>
2023-06-03 10:39:08 +0200

    removed commented code

--- src/main/java/com/melompk/database/DownloadUtils.java
+++ src/main/java/com/melompk/database/DownloadUtils.java
@@ -35,34 +35,17 @@ public class DownloadUtils {//Model
         }
     }
     public static boolean IsSongDownloaded(String filename){
-        // return Files.exists(Paths.get(new File("").getAbsolutePath() + "/src/main/resources/Songs/"+ filename));
         return DownloadedSongs.contains(filename);
     }
     public static boolean IsCoverDownloaded(String filename){
-        // return Files.exists(Paths.get(new File("").getAbsolutePath() + "/src/main/resources/Covers/"+ filename));
         return DownloadedCovers.contains(filename);
     }
     public static void Clear() throws IOException {
-        // File songsDirectory = Files.createDirectories(Paths.get("src/main/resources/Songs")).toFile();
-        // if (songsDirectory.exists()) {
-        //     File[] files = songsDirectory.listFiles();
-        //     if (files != null) {
-        //         for (File file : files) {
-        //             file.delete();
-        //         }
-        //     }
-        // }
+        
         while(!DownloadedSongs.isEmpty()) {
             Paths.get("src/main/resources/Songs/" + DownloadedSongs.getLast()).toFile().delete();
             DownloadedSongs.removeLast();
         }
-        // File coversDirectory = Files.createDirectories(Paths.get("src/main/resources/Covers")).toFile();
-        // File[] files = coversDirectory.listFiles();
-        // if (files != null) {
-        //     for (File file : files) {
-        //         file.delete();
-        //     }
-        // }
         while(!DownloadedCovers.isEmpty()) {
             Paths.get("src/main/resources/Covers/" + DownloadedCovers.getLast()).toFile().delete();
             DownloadedCovers.removeLast();
